fd0455cee05788ddac835f872ca0f8bdf1bffab0
guillem <guillem.cordoba@gmail.com>
2017-09-27 23:32:59 +0200

    Created container and countdown components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountdownComponentComponent } from './countdown-component/countdown-component.component';
+import { ContainerComponentComponent } from './container-component/container-component.component';
+import { ContainerComponent } from './container/container.component';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownComponentComponent,
+    ContainerComponentComponent,
+    ContainerComponent,
+    CountdownComponent
   ],
   imports: [
     BrowserModule
