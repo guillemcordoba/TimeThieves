48f11398d1d3f7954398b7a1b2961b121c2a5b04
guillem <guillem.cordoba@gmail.com>
2017-10-04 22:42:00 +0200

    Prepared for electron development

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+path = require('path');
+url = require('url');
+
+// Executes when the application 
+// is initialized.
+app.on('ready', function() {
+    console.log('Starting application!');
+    mainWindow = new BrowserWindow({ width: 1280, height: 960 });    
+    // Change loadUrl to load index.html
+    // using url and path package 
+    // to format the file url
+    mainWindow.loadURL(url.format({
+        //__dirname is the current working dir
+        pathname: path.join(__dirname, 'dist', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // It is useful to open dev tools
+    // for debug.
+    //mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
+
+// Defines the behavior on close.
+app.on('window-all-closed', function() {
+    app.quit();
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CountdownComponentComponent } from './countdown-component/countdown-component.component';
-import { ContainerComponentComponent } from './container-component/container-component.component';
-import { ContainerComponent } from './container/container.component';
 import { CountdownComponent } from './countdown/countdown.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountdownComponentComponent,
-    ContainerComponentComponent,
     ContainerComponent,
     CountdownComponent
   ],
